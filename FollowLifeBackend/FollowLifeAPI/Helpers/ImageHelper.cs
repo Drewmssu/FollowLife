adea5f22c1a000385c08d19cb35cdb98bfdba114
Drewmsu <fran.mp07@gmail.com>
2018-04-18 11:43:45 -0500

    Image Helper

--- FollowLifeBackend/FollowLifeAPI/Helpers/ImageHelper.cs
+++ FollowLifeBackend/FollowLifeAPI/Helpers/ImageHelper.cs
@@ -1,13 +1,39 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 
 namespace FollowLifeAPI.Helpers
 {
     public static class ImageHelper
-    { 
-        //public static string GetImageURL(string fileName) => string.IsNullOrEmpty(fileName) ? "" 
-        
+    {
+        public static string GetImageURL(string fileName) => string.IsNullOrEmpty(fileName) ? null : HttpContext.Current.Server.MapPath("~\\Upload\\ProfileImages\\" + fileName);
+
+        public static string UploadImage(HttpPostedFile file)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri(HttpContext.Current.Server.MapPath("~\\Upload\\ProfileImages\\")),
+                    Method = HttpMethod.Post,
+                    Content = new StreamContent(file.InputStream)
+                };
+
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+                using (var response = client.SendAsync(request).Result)
+                {
+                    using (var content = response.Content)
+                    {
+                        return JsonConvert.DeserializeObject<string>(content.ReadAsStringAsync().Result);
+                    }
+                }
+            }
+        }
+
     }
 }
